8ad6f770c559a7f40e4203b7da82a336e8f85744
ak-you2 <atsushi.komura@you2.jp>
2025-06-28 10:08:45 +0900

    Add linter.

--- aws-quiz-app/types/next-auth.d.ts
+++ aws-quiz-app/types/next-auth.d.ts
@@ -1,18 +1,20 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-    }
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    uid?: string
+    token: {
+      uid?: string;
+    };
   }
 }
