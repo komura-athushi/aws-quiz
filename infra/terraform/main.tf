477bb68890b293858ee23ea2578a4755950e044d
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2025-06-30 23:20:45 +0900

    Fix terraform version

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,10 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.80.0"
+      version = "6.0.0"
     }
     random = {
       source  = "hashicorp/random"
+      version = "3.7.2"
     }
   }
 }
